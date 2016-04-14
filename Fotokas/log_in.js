9b84cac0a5399b358b0b5d0099bc59e709ae3bd7
Margus Sumla <msumla@itcollege.ee>
2016-04-14 21:30:36 +0300

    New stuff

---
+++ Fotokas/log_in.js
@@ -0,0 +1,15 @@
+document.querySelector(".login-show").addEventListener("click",
+    function(event){
+        document.querySelector(".logform").style.display="block";
+        document.querySelector("#logintoggle1").style.display="none";
+        document.querySelector("#logintoggle0").style.display="block";
+    }
+);
+
+document.querySelector(".login-hide").addEventListener("click",
+    function(event){
+        document.querySelector(".logform").style.display="none";
+        document.querySelector("#logintoggle0").style.display="none";
+        document.querySelector("#logintoggle1").style.display="block";
+    }
+);
